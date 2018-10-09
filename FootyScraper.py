4c4dcb1283ec6c98e4910eb30b4f04d093f378f6
lhatfield <lachlanhatfield.lh@gmail.com>
2018-10-10 09:56:16 +1100

    Update, resolved URL issues, New bugs found
    Issues in ScrapeBasicMatchTables, where BS cant find sortabletable0

--- FootyScraper.py
+++ FootyScraper.py
@@ -5,11 +5,15 @@ from bs4 import BeautifulSoup
 import requests
 import lxml
 
+
+
 def main():
+    print('starting')
     ScrapeSeasonsInRange(2017,2017)
 
 #loop through years to be scraped
 def ScrapeSeasonsInRange(start,end):
+    print('ScrapeSeasons In Range From ' + str(start) + ' to ' + str(end))
     CurrentUrlArray = []
     current = start
     url = 'https://afltables.com/afl/seas/'
@@ -18,14 +22,19 @@ def ScrapeSeasonsInRange(start,end):
     while current <= end:
         #gets all games from given season(at start)
         ScrapeMatchURLFromSeason(url+str(current)+'.html', CurrentUrlArray)
-        FormatBasicUrltoComplete(CurrentUrlArray)
+        CurrentUrlArray = CreateURLArray(CurrentUrlArray)
+        for a in CurrentUrlArray:
+            ScrapeBasicMatchTables(a)
+        current = current + 1
         
-#Format game         
-def FormatBasicUrltoComplete(CurrentUrlArray):
+#Format game from Season game page
+def CreateURLArray(CurrentUrlArray):
     matchurl = 'https://afltables.com/afl'
+    newURLArray = []
     for a in CurrentUrlArray:
-        a = matchurl + a[2:100]
-        print('formatting '+ a)
+        newURLArray.append(matchurl + a[2:100])
+    CurrentUrlArray = newURLArray
+    return newURLArray
        
 #Create an array of URLs for scraping
 def ScrapeMatchURLFromSeason(url,CurrentUrlArray): 
@@ -34,19 +43,35 @@ def ScrapeMatchURLFromSeason(url,CurrentUrlArray):
     for a in soup.find_all('a',href=True) :
         if '/stats/games/' in a['href']:
             CurrentUrlArray.append(a['href'])
-
-
-def ScrapebasicMatchTables(url):
+          
+def ScrapeBasicMatchTables(url):
+    print('Scraping ' + url)
     source = requests.get(url).text
-    soup = BeautifulSoup(source,'l xml')
-    ScrapeTeamStats(soup.find('sortableTable0'))
-    ScrapeTeamStats(soup.find('sortableTable1'))
+    soup = BeautifulSoup(source,'lxml')
+    
+    #ScrapeMatchInfo(soup.find('tbody'))
+    print(soup.find('table',id='sortabletable0'))
+    ScrapeTeamStats(soup.find('table',id='sortabletable0'))
+   # ScrapeTeamStats(soup.find('sortableTable1'))
     #Get scoring progression by table here
+
+def ScrapeMatchInfo(MatchTable):
+    soup = BeautifulSoup(MatchTable, 'lxml')
+    information = soup.findall('td')[1]
+    print(information)
+    
+    #roundNum = information.split[1,1]
+    #ground = 0
+    #attendance= 0
+    #date= 0
+    
      
 def ScrapeTeamStats(team):
-    for player in team.find_all('div',class_='tr'):
-        #add player URL
-        ScrapePlayerRow(player)
+    source = team.text
+    soup = BeautifulSoup(source,'lxml')
+    for player in soup.find_all('tr'):#add player URL
+        print('player ' + player) 
+        #ScrapePlayerRow(player)
     
 def ScrapePlayerRow(player):
     stats = []
@@ -57,9 +82,9 @@ def ScrapePlayerRow(player):
             stats.append(stat)
         else:
             stats.append(0) #AFL Tables if they did not record a stat there is no 0
-    PlayerID = PlayerInDB(playerURL)
-    if PlayerID == -1:
-        player[0]
+    #PlayerID = PlayerInDB(playerURL)
+   # if PlayerID == -1:
+     #   player[0]
          #create new entry for player
          
          
@@ -81,12 +106,14 @@ def PlayerInDB(playerURL): #return id
     #create request for player information
     #get name and DOB, limitation, if two players have the ame name and were born on the same day
     playerName = '<name>'
-    DOB = '<xx/yy/zzzz>
+    DOB = '<xx/yy/zzzz>'
     #Query to database to find 
-    query = 'SELECT playerID FROM PLAYER WHERE name ===' + player ' AND DOB === ' + DOB'
-    if query == -1 #if err/ not found
+    query = 'SELECT playerID FROM PLAYER WHERE name ===' + player + ' AND DOB ===' + DOB
+    #exec query
+    query = 0
+    if query < 0: #if err/ not found
        query = CreateNewPlayerEntry(playerName, DOB)
     return query
 
     
-  
+main();
