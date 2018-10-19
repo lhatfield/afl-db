4b61d316e00a3857cdc4305563bcd30bbc077b25
lhatfield <lachlanhatfield.lh@gmail.com>
2018-10-19 23:44:59 +1100

    Updated, solved problems just need to filter throught the table
    solved the problem by taking all 'tbody' out of the page and only getting number 1 and 2. It works and gets the relevant team data. I'm not going to tempt fate.

--- FootyScraper.py
+++ FootyScraper.py
@@ -1,5 +1,6 @@
 #Script to scrape season information to populate AFL DB
 #Created with help from https://youtu.be/ng2o98k983k
+#Tutorial starts @ 9.14
 
 from bs4 import BeautifulSoup
 import requests
@@ -48,81 +49,25 @@ def ScrapeBasicMatchTables(url):
     print('Scraping ' + url)
     source = requests.get(url).text
     soup = BeautifulSoup(source,'lxml')
-    tables = soup.find_all('table')
-    print('asdf')
-    for a in tables:
-        print('herio')
-        print(str(a.prettify))
-        print('-----------------------------------------------')
-    #home = tables[4].find('tbody')
-    #ScrapeTeamStats(home)
-    #away = tables[2].find('tbody')
-    #ScrapeTeamStats(away)
-    
-   # scoring = tables[6].text
-  
+    #print('asdf')
+    tableNo = 0
+    #print(soup.prettify())
+    for a in soup.find_all('tbody'):
+        tableNo = tableNo + 1
+        
+       # print(a)
+        if tableNo == 1 or tableNo == 2:
+            print(tableNo)
+            ScrapePlayerDataMatchTable(str(a))
+            print('-----------------------------------------------')
      
-def ScrapeTeamStats(team):
-    soup = BeautifulSoup(team, 'lxml')
-    print('team' + team)
-    print(' ')
-    print(' ')
-    print('------------------------------------------------------------------ ')
-    for player in soup.find_all('tr'):#add player URL
-        print('player ' + player) 
-        #ScrapePlayerRow(player)
-    
-def ScrapePlayerRow(player):
-    stats = []
-    #append the player page to the stat line
-    stats[0] = player.find('a',href=True)
-    for stat in player.find_all('div',class_='td'):
-        if len(stat) > 0:
-            stats.append(stat)
-        else:
-            stats.append(0) #AFL Tables if they did not record a stat there is no 0
-    #PlayerID = PlayerInDB(playerURL)
-   # if PlayerID == -1:
-     #   player[0]
-         #create new entry for player
-         
-         
-    #Insert array data into database            
-    
-def CreateNewPlayerEntry(playerName, DOB):
-    #connect to database/ check connection
-    #player ID auto increments
-    #Name, DOB, Height, Weight, Armspan
-    newPlayerQuery = 'INSERT INTO Player VALUES'+ playerName + ' ' + DOB
-    findPlayerId = 'SELECT playerID from Player WHERE playerName === ' + playerName + ' AND DOB === ' + DOB
-    
-    #exec queries
-    playerID = 123
-    return playerID
-    
+   
+def ScrapePlayerDataMatchTable(table):
+    #Have the two stats tables for all players
+    print(table) 
+    i = 13 #index
+    for c in table:
+        
+        
 
-def PlayerInDB(playerURL): #return id
-    #create request for player information
-    #get name and DOB, limitation, if two players have the ame name and were born on the same day
-    playerName = '<name>'
-    DOB = '<xx/yy/zzzz>'
-    #Query to database to find 
-    query = 'SELECT playerID FROM PLAYER WHERE name ===' + player + ' AND DOB ===' + DOB
-    #exec query
-    query = 0
-    if query < 0: #if err/ not found
-       query = CreateNewPlayerEntry(playerName, DOB)
-    return query
-
-
-def ScrapeMatchInfo(MatchTable):
-    soup = BeautifulSoup(MatchTable, 'lxml')
-    information = soup.findall('td')[1]
-    print(information)
-    
-    #roundNum = information.split[1,1]
-    #ground = 0
-    #attendance= 0
-    #date= 0
-    
 main();
